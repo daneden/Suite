a589a70d5c6eedc7f7be29f95a649c3345ccd527
Daniel Eden <dan.eden@me.com>
2025-08-27 20:20:11 +0100

    Refactor the floatingOverlay modifier to more closely resemble the safeAreaBar API and add the new backportCircleSymbolVariant method

---
+++ Sources/Suite/View+backportCircleSymbolVariant.swift
@@ -0,0 +1,19 @@
+//
+//  View+backportCircleSymbolVariant.swift
+//  Suite
+//
+//  Created by Daniel Eden on 27/08/2025.
+//
+
+import SwiftUI
+
+@available(macOS 12, iOS 15.0, tvOS 15.0, watchOS 8.0, *)
+extension View {
+	func backportCircleSymbolVariant() -> some View {
+		if #available(iOS 26, macOS 26, visionOS 26, *) {
+			return self
+		} else {
+			return self.symbolVariant(.circle)
+		}
+	}
+}
--- Sources/Suite/View+floatingOverlay.swift
+++ Sources/Suite/View+floatingOverlay.swift
@@ -11,16 +11,33 @@ import SwiftUI
 private struct FloatingOverlayModifier<OverlayContent: View>: ViewModifier {
 	@State private var size = CGSize.zero
 	
-	var alignment: Alignment
-	var insetEdges: Edge.Set
+	var edge: VerticalEdge
+	var alignment: HorizontalAlignment
+	var spacing: CGFloat?
 	
 	@ViewBuilder var overlayContent: OverlayContent
 	
+	var insetEdge: Edge.Set {
+		switch edge {
+		case .bottom: return .bottom
+		case .top: return .top
+		}
+	}
+	
+	var resolvedAlignment: Alignment {
+		switch alignment {
+		case .leading: return .leading
+		case .trailing: return .trailing
+		default: return .center
+		}
+	}
+	
 	func body(content: Content) -> some View {
 		content
-			.contentMargins(insetEdges, size.height, for: .automatic)
-			.overlay(alignment: alignment) {
+			.contentMargins(insetEdge, size.height, for: .automatic)
+			.overlay(alignment: resolvedAlignment) {
 				self.overlayContent
+					.padding(insetEdge, spacing)
 					.overlay {
 						GeometryReader { g in
 							Color.clear
@@ -35,21 +52,30 @@ private struct FloatingOverlayModifier<OverlayContent: View>: ViewModifier {
 public extension View {
 	/// Layers the view that you specify in front of this view, with content margins applied to scrolling containers.
 	/// - Parameters:
-	///   - alignment: The alignment for the foreground content
-	///   - insetEdges: The container edges to indent
-	///   - content: The content to overlay
-	/// - Returns: A view that uses the specified content as a foreground, with content margins equal to the overlaid content size applied to the specified inset edges.
-	func floatingOverlay<OverlayContent: View>(
-		alignment: Alignment = .bottom,
-		insetEdges: Edge.Set = .bottom,
+	///   - edge: The vertical edge of the view on which content is placed
+	///   - alignment: The alignment guide used to position `content` horizontally
+	///   - spacing: Extra distance placed between the two views, or nil to use the default amount of spacing.
+	///   - content: A view builder function providing the view to display as a custom bar.
+	/// - Returns: A new view that displays content above or below the modified view, making space for the content view by vertically insetting the modified view, adjusting the safe area and scroll edge effects to match.
+	func backportSafeAreaBar<OverlayContent: View>(
+		edge: VerticalEdge = .bottom,
+		alignment: HorizontalAlignment = .center,
+		spacing: CGFloat? = nil,
 		@ViewBuilder content: () -> OverlayContent
 	) -> some View {
-		modifier(
-			FloatingOverlayModifier(
-				alignment: alignment,
-				insetEdges: insetEdges,
-				overlayContent: content
+		if #available(iOS 26, macOS 26, visionOS 26, watchOS 26, *) {
+			return safeAreaBar(edge: edge, alignment: alignment) {
+				content()
+			}
+		} else {
+			return modifier(
+				FloatingOverlayModifier(
+					edge: edge,
+					alignment: alignment,
+					spacing: spacing,
+					overlayContent: content
+				)
 			)
-		)
+		}
 	}
 }
